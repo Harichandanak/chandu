4cbe2423d37c6a3babcf930f683f28a3be5df09a
Harichandanak <41770111+Harichandanak@users.noreply.github.com>
2018-09-27 16:14:04 +0530

    Create prgm3.py

---
+++ prgm3.py
@@ -0,0 +1,8 @@
+value=input()
+if value>='a' and value<='z' or value>='A' and value<='Z':
+  if value=='a' or value=='e' or value=='i' or value=='o' or value=='u' or value=='A' or value=='E' or value=='I' or value=='O' or value=='U':
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("invalid")
