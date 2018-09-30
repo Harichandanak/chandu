2f36dc1bd11dcb6939dcfc35219217bc52d5f78f
Harichandanak <41770111+Harichandanak@users.noreply.github.com>
2018-09-30 15:36:46 +0530

    Create program30.py

---
+++ program30.py
@@ -0,0 +1,10 @@
+j1,k1=input("").split()
+j1,k1=int(j1),int(k1)
+j2,k2=input("").split()
+j2,k2=int(j2),int(k2)
+m1=(j1*60)+k1
+m2=(j2*60)+k2
+tmin=abs(11-12)
+k=tmin%60
+hr(tmin-k)//60
+print (int(hrs).int(k))
