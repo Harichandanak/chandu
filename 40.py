eab5af7db5cd389c5aede51b4e136141989e686c
Harichandanak <41770111+Harichandanak@users.noreply.github.com>
2018-09-30 17:40:48 +0530

    Create 40.py

---
+++ 40.py
@@ -0,0 +1,15 @@
+g=int(input(" "))
+e=0
+f=1
+count=0
+if(g==0):
+  print(e)
+elif(g<0):
+  print("positive number")
+else:
+  while(counr<g):
+    print(e)
+    nexterm=e+f
+    e=f
+    f=nexterm
+    count+=1
